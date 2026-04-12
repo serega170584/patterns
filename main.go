bfbef37772652f8ab672d0b4a5ff9c455ba2965b
krivobokov <krivobokov_sv@magnit.ru>
2026-04-12 14:19:51 +0300

    pprof add

--- main.go
+++ main.go
@@ -3,12 +3,22 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"math/rand"
+	"net/http"
+	_ "net/http/pprof"
 	"patterns/ratecounter"
 	"time"
 )
 
 func main() {
+	go func() {
+		fmt.Println("Pprof server started on :6060")
+		if err := http.ListenAndServe("localhost:6060", nil); err != nil {
+			log.Fatalf("Pprof server failed: %v", err)
+		}
+	}()
+
 	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 
